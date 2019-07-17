0d30070b9e7e06b37b0c1f171cf49eb7af69f4ef
rbr7 <52232895+rbr7@users.noreply.github.com>
2019-07-17 22:48:11 +0530

    Add files via upload

---
+++ projects/best.R
@@ -0,0 +1,55 @@
+best <- function(state, outcome) {
+  
+  # Read outcome data
+  outcome_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  ##str(outcome_data)
+  ##head(outcome_data)
+  
+  outcome <- tolower(outcome)
+  
+  # Checking that state and outcome are valid
+  x = state %in% outcome_data$State
+  
+  outcomes <- c("heart attack","heart failure","pneumonia")
+  y <- outcome %in% outcomes
+  
+  if(x == FALSE)
+    stop("invalid state")
+  if(y == FALSE)
+    stop("invalid outcome")
+  
+  
+  #Filter by state
+  sub_data <- subset(outcome_data, State == state)
+  ##head(sub_data)
+  
+  # Columns indices to keep
+  if(outcome == "heart attack"){
+    sub_col <- sub_data$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack
+  }
+  if(outcome == "heart failure"){
+    sub_col = sub_data$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure
+  }
+  if(outcome == "pneumonia"){
+    sub_col <- sub_data$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia
+  }
+  
+  # Filtering out extra data 
+  Hospital_Name <- sub_data$Hospital.Name
+  Mortality_rate <- as.numeric(sub_col)
+  
+  df <- data.frame(Hospital_Name, Mortality_rate)
+  
+  # Removing missing values to keep numerical ones
+  df <- df[!is.na(df$Mortality_rate),]
+  
+  min_rate <- min(df$Mortality_rate, na.rm = TRUE)
+  
+  result_df <- df[df$Mortality_rate == min_rate, ]
+  
+  # Order Column to Top 
+  result_df <- result_df[order(result_df$Hospital_Name), ]
+  
+  print(as.character(result_df[, "Hospital_Name"][1]))
+
+}
